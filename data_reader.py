f5c041c9e2f8c18f8aeb9633a2f488648264495b
Alisa Mironenko <36305465+torpidfox@users.noreply.github.com>
2018-04-18 23:38:39 +0300

    wrapped data provider

--- data_reader.py
+++ data_reader.py
@@ -1,17 +1,41 @@
 import numpy as np
-from random import shuffle
 
-def _pad(arr, batch_size, padding_val = 20):
-	result = np.append(arr, arr[:padding_val-batch_size, :], axis=0)
-	return result
+class Data:
+	""" class preparing feeding data for model """
 
-def _parse_text_file(batch_size, file, batch_count):
-    with open(file, 'r') as f:
-        result = [_pad(np.asarray([[np.float64(el) for el in f.readline().split()] for _ in range(batch_size)]), batch_size) for _ in range(batch_count)]
+	def __init__(filenames=[], batch_size=200, padding_size=1):
+		"""
+		keywords args:
+		filenames -- source files
+		batch_size -- actual batch size for dataset
+		padding_size -- how many batch_size pieces are required to pad batch for model
+		"""
 
-    shuffle(result)
-    return result
+		self.filenames = filenames
+		self.batch_size = batch_size
+		self.padding_size = padding_size
+		self.file = None
+		self.dim = 0
 
-def construct_dataset(batch_size, filenames, batch_count=1):
-    for file in filenames:
-        yield _parse_text_file(batch_size, file, batch_count)
+	def _load_next(self):
+		"""load next file"""
+
+		with open(self.filenames.pop(), 'r') as f:
+			lines = np.asarray([l.split() for l in f.readlines()])
+			self.dim = len(lines[0])
+			file = [np.asfloat(line) for line in lines]
+
+		batch_count = len(file) % self.batch_size
+		self.file = iter(np.reshape(file, (self.batch_size, batch_count)))
+
+	def count(self):
+		"""get sample size"""
+		return self.dim
+
+	def __next__(self):
+		"""get next batch"""
+
+		if self.pos == len(self.file):
+			_load_next()
+
+		return np.flatten(self.next() * padding_size)
