f81f3e45a87a2196cd3ffc7da3bdc96f7b24712d
Alisa Mironenko <nonexistence42@gmail.com>
2019-09-18 21:15:06 +0300

    added scatterplotting script

--- scatterplotter.py
+++ scatterplotter.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np 
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn.decomposition import PCA
+import pandas as pd
 
 fig = plt.gcf()
 
@@ -19,39 +20,35 @@ def plot(x, y, title, pos, xl='Original', yl='Decoded'):
 	# plt.ylim(bottom=0, top=14)
 	plt.plot(ax.get_xlim(), ax.get_ylim(), ls="--", c=".1") 
 
-with np.load('model0_res_valid.npz') as f: 
-	d1 = f['arr_0'] 
+with np.load('../logs/two_sets_from_one/model0_res_valid.npz') as f: 
+	d1 = f['second_set_original']
+	print(np.shape(f['original'])) 
 	print(np.shape(d1))
-	d2_1 = f['arr_3'] 
+
+with np.load('../logs/two_sets_from_one/model1_res_valid.npz') as f: 
+	d2_1 = f['second_set'] 
+	print(np.shape(d2_1))
 	means = [((x - y) ** 2).mean() for x, y in zip(d1, d2_1)]
-	print(means)
-	print(np.asarray(means).mean())
-	plot(d1[2], d2_1[2], "Main dataset valid result", 3)
+	plot(d1[1], d2_1[1], "Second split decoder applied to first split data", 3)
 
 
-with np.load('model0_res_valid.npz') as f:
-	d1 = f['arr_0'] 
+with np.load('../logs/two_sets_from_one/model0_res_valid.npz') as f:
+	d1 = f['original'] 
 	print(np.shape(d1))
 	plot(d1[1], d1[15], "Main dataset random samples", 1, xl='Sample 1', yl='Sample 2')  
 
-with np.load('model1_res_valid.npz') as f: 
-	d1 = f['arr_0'] 
+with np.load('../logs/two_sets_from_one/model1_res_valid.npz') as f: 
+	d1 = f['original'] 
 
 	print(np.shape(d1)) 
 	plot(d1[1], d1[10], "Additional dataset random samples", 2, xl='Sample 1', yl='Sample 2')
 
-with np.load('model1_res_valid.npz') as f:
-	d1 = f['arr_0'] 
-	print(np.shape(d1))
-	d2 = f['arr_3'] 
+
+with np.load('../logs/two_sets_from_one/model1_res_valid.npz') as f: 
+	d2 = f['decoded']
+	d1 = f['original'] 
 	print(np.shape(d2)) 
 	plot(d1[1], d2[1], "Additional dataset valid result", 4)
-
-# with np.load('multi_sets2/model1_res_control.npz') as f:
-# 	data2_in= f['arr_0']
-
-# with np.load('multi_sets2/model0_res_control.npz') as f:
-# 	data1_in= f['arr_0']
 
 
 # with np.load('multi_sets2/model2_res_train.npz') as f:
